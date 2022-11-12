ffafc3354c9e29a3b75daf7f2ea221b1e3cccf40
stiffmeister925 <113961079+stiffmeister925@users.noreply.github.com>
2022-11-12 12:37:51 +0800

    fix: Update Middleware
    modified variable value

--- express-with-mongoose/middleware/CheckIfUserExist.js
+++ express-with-mongoose/middleware/CheckIfUserExist.js
@@ -2,7 +2,7 @@ import User from '../models/User.js'
 
 export default async function CheckIfUserExist(req, res, next) {
 
-  const id = req.params.id;
+  const id = req.header("X-User-ID");
   const findUser = await User.findOne({ _id: id });
 
   if (!findUser) {
